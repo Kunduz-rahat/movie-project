1001e137c1f9bad11baf9a756a73c53a331b245d
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-02-17 23:45:34 +0600

    correction actorList

--- src/views/AboutPerson/index.js
+++ src/views/AboutPerson/index.js
@@ -14,8 +14,8 @@ const AboutPerson = () => {
   const {films, isLoadingFilms} = useSelector(s => s.films)
   const {id} = useParams()
 
-  useEffect(() => dispatch(getInfoActor(id)), [id])
-  useEffect(() => dispatch(getActorFilms(id)), [id])
+  useEffect(() => dispatch(getInfoActor(id)), [])
+  useEffect(() => dispatch(getActorFilms(id)), [])
 
   if (isLoading && isLoadingFilms) {
     return <div className='d-flex justify-content-center'>
